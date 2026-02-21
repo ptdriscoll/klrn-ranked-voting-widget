02274eef79b47a12aea854f1a1660ee97d24fd14
Patrick Driscoll <pdriscoll@satx.rr.com>
2026-02-21 16:05:51 -0600

    Add insert, reset and drop statements

--- server/reference.sql
+++ server/reference.sql
@@ -1,6 +1,18 @@
 -- Reference SQL for schema and queries
 -- This file is not executed directly by the application
 
+--------------------------------------------------
+-- reset or drop
+--------------------------------------------------
+
+TRUNCATE TABLE vote_sessions, vote_results;
+
+DROP TABLE vote_sessions, vote_results;
+
+--------------------------------------------------
+-- create
+--------------------------------------------------
+
 CREATE TABLE vote_sessions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     token_hash CHAR(64) NOT NULL,
@@ -23,3 +35,25 @@ CREATE TABLE vote_results (
     INDEX(vote_session_id),
     INDEX(entry_id)
 );
+
+--------------------------------------------------
+-- insert
+--------------------------------------------------
+
+INSERT INTO vote_sessions
+(token_hash, zip, ip_address, fingerprint_hash, created_at)
+VALUES
+(
+  'abc',
+  '78247',
+  '192.168.1.100',
+  'xyz',
+  '2026-02-17 14:23:11'
+);
+
+INSERT INTO vote_results (vote_session_id, entry_id, points) VALUES
+(1, 4, 16),
+(1, 2, 14),
+(1, 5, 12),
+(1, 1, 1),
+(1, 3, 1);
