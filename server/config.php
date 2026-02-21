5a63216313332ebcae5197bf9dd325d5afc5e247
Patrick Driscoll <pdriscoll@satx.rr.com>
2026-02-21 11:28:06 -0600

    Add config data

---
+++ server/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  'db_host' => 'localhost',
+  'db_user' => 'root',
+  'db_pass' => '',
+  'db_name' => 'klrn_city_showdown',
+  'timezone' => 'America/Chicago',  
+	'users' => ['dev1' => '123', 
+              'dev2' => '234']
+);
